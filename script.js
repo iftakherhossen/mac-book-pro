7c75f74738dd0bd385e77a18d0c9220a638301ff
iftakherone <54303402+iftakherone@users.noreply.github.com>
2021-08-21 15:01:16 +0600

    adding js for calculation

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
-// for changing the price of memory cost
-document.getElementById('memory-one').addEventListener('click', function () {
-    document.getElementById('memory-cost').innerText = "0";
-});
-document.getElementById('memory-two').addEventListener('click', function () {
-    document.getElementById('memory-cost').innerText = "180";
-});
+function priceVariant(productSelector, priceSelector, price) {
+    document.getElementById(productSelector).addEventListener('click', function () {
+        document.getElementById(priceSelector).innerText = price;
 
-// for changing the price of storage cost
-document.getElementById('storage-one').addEventListener('click', function () {
-    document.getElementById('storage-cost').innerText = "0";
-});
-document.getElementById('storage-two').addEventListener('click', function () {
-    document.getElementById('storage-cost').innerText = "100";
-});
-document.getElementById('storage-three').addEventListener('click', function () {
-    document.getElementById('storage-cost').innerText = "180";
-});
+        console.log(productSelector, priceSelector, price);
+        totalPriceCalculate();
+    });
+}
 
-// for changing the price of delivery cost
-document.getElementById('free-delivery').addEventListener('click', function () {
-    document.getElementById('delivery-cost').innerText = "0";
-});
-document.getElementById('delivery-charge').addEventListener('click', function () {
-    document.getElementById('delivery-cost').innerText = "20";
-});
+function totalPriceCalculate() {
+    let priceSelector = document.getElementsByClassName('price');
+    let sum = 0;
 
+    for (let elements of priceSelector) {
+        let priceText = elements.innerText;
+        let price = parseInt(priceText);
+
+        sum = sum + price;
+    }
+
+    document.getElementById('total-price').innerText = sum;
+    document.getElementById('final-price').innerText = sum;
+}
+
+function couponPriceTotal() {
+    const couponCode = 'stevekaku';
+
+    document.getElementById('apply-btn').addEventListener('click', function () {
+        const promoCost = document.getElementById('promo-code-input').value;
+        const totalPrice = document.getElementById('total-price').innerText;
+        let finalPrice = 0;
+
+        console.log(totalPrice)
+        if (promoCost == couponCode) {
+            finalPrice = totalPrice * 0.8;
+        }
+        else {
+            window.alert('Invalid! Coupon Code, Try again!')
+        }
+
+        document.getElementById('final-price').innerText = finalPrice;
+    })    
+}
+
+
+
+priceVariant('memory-one', 'memory-cost', 0);
+priceVariant('memory-two', 'memory-cost', 180);
+priceVariant('storage-one', 'storage-cost', 0);
+priceVariant('storage-two', 'storage-cost', 100);
+priceVariant('storage-three', 'storage-cost', 180);
+priceVariant('free-delivery', 'delivery-cost', 0);
+priceVariant('delivery-charge', 'delivery-cost', 20);
+
+couponPriceTotal();
